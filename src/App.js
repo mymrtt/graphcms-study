dc6101848d76efb8478241ac602325134a65e55d
mymrtt <mymrtt@gmail.com>
2021-07-20 16:08:46 -0300

    refactor

--- src/App.js
+++ src/App.js
@@ -2,52 +2,58 @@
 import React, { useState, useEffect } from 'react';
 import { request } from 'graphql-request';
 
-// Images
-import lampImg from '../src/assets/lamp.jpeg';
-
 // Styles
 import './App.css';
 
-function App() {
-  const [data, setData] = useState(undefined);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const { site } = await request(
-        'https://api-eu-central-1.graphcms.com/v2/ckg825dbrqesa01z5hvx23e8b/master',
-        
-        `
-          {
-            site(where: {id: "ckg838qe00b910104n8owh7he"}) {
-              page {
-                fragmento {
-                  name
-                  singleText
-                }
-              }
+export async function getData() {
+  const { site } = await request(
+    'https://api-eu-central-1.graphcms.com/v2/ckg825dbrqesa01z5hvx23e8b/master',
+    
+    `
+      {
+        site(where: {id: "ckg838qe00b910104n8owh7he"}) {
+          page {
+            fragmento {
+              id
+              name
+              singleText
             }
           }
-        `
-      )
+        }
+      }
+    `
+  )
 
-      setData(site);
+  if(!site.page.length) return null;
+
+  return site.page;
+}
+
+export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getData();
+
+      if (data) {
+        setData(data)
+      }
     }
+
     fetchData();
   }, []);
 
-
   const renderList = () => {
-    return (
-      data && data.page.map(item => (
-        <div className="container_item">
-          <h2>{item.fragmento.singleText[0]}</h2>
-          {item.fragmento.singleText[0] === 'Suspendisse sit amet erat ex' 
-            ? <img src={lampImg} alt="Lâmpada" className="lampImg" />
-            : <p>{item.fragmento.singleText[1]}</p>
-          }
-        </div>
-      ))
-    )
+    return data?.map(({ fragmento }) => (
+      <div 
+        key={fragmento?.id}
+        className="card" 
+      >
+        <h2>{fragmento?.singleText[0]}</h2>
+        <p>{fragmento?.singleText[1]}</p>
+      </div>
+    ))
   }
 
   return (
@@ -56,5 +62,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
